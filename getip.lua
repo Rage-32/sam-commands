b5375d6a10a6c10dcdf629128deba0db9d38c9fc
rage-69 <45928149+rage-69@users.noreply.github.com>
2021-04-12 20:50:20 -0400

    Create getip.lua

---
+++ getip.lua
@@ -0,0 +1,20 @@
+command.new("getip")
+	:SetPermission("getip")
+
+	:Help(language.get("getip_help"))
+	:DisableNotify(true)
+	:MenuHide(true)
+	:AddArg("player", {
+        single_target = true, 
+  --      cant_target_self = true, 
+    })
+
+	:OnExecute (function(ply, targets)
+		for i = 1, #targets do
+            local target = targets[i]
+			sam.player.send_message(ply, target:Name() .. ": " .. target:IPAddress())
+		end
+	
+			if sam.is_command_silent then return end
+	end)
+:End()

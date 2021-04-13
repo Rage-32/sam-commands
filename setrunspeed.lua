a56159b559136eb97d6dfda70bd4d11cb378cbf6
rage-69 <45928149+rage-69@users.noreply.github.com>
2021-04-12 23:25:42 -0400

    Create setrunspeed.lua

---
+++ setrunspeed.lua
@@ -0,0 +1,27 @@
+command.new("setrunspeed")
+	:SetPermission("setrunspeed")
+
+	:Help(language.get("setrunspeed_help"))
+	:AddArg("player", {
+        single_target = true, 
+    })
+	:AddArg("number", {
+        optional = true, -- makes it optional!
+        default = 600, -- add a default amount if there was no input!
+        hint = "amount", -- ummmm?
+        min = 0, -- input < 100 then the input gonna be 100
+        max = 500000000, -- input > 200 then the input gonna be 200
+        round = true, -- numbers like 1.5, 1.4, 1.2 gets rounded
+    })
+
+	:OnExecute (function(ply, targets, amount)
+        for i = 1, #targets do
+            local target = targets[i]
+			target:SetRunSpeed( amount )
+			sam.player.send_message(nil, "{A} set {T}'s runspeed to {V}", {
+				A = ply, T = targets, V = amount
+			})
+		end
+
+	end)
+:End()
